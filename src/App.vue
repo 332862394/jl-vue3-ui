6f248bc02e03cf4c8a7e562b5ab0559ad9fb49d4
luzh <332862394@qq.com>
2022-01-04 20:16:05 +0800

    初始化router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloW2 from './components/HelloW2.vue'
+import HelloW2 from "./components/HelloW2.vue";
 </script>
 
 <template>
-  <div>hi</div>
-  <HelloW2/>
+  <div>导航栏</div>
+  <router-link to="/">hellow</router-link>|
+  <router-link to="/xxx">test</router-link>
+  <hr />
+  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Hellow from './components/Hellow2.vue'
+import test from './components/tese2.vue'
 
-createApp(App).mount('#app')
+const history = createWebHashHistory()
+const router = createRouter({
+    history: history,
+    routes: [{ path: '/', component: Hellow },
+    { path: '/xxx', component: test }]
+})
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
