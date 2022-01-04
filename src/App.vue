94d0ba35ce20c55bfd5143e60f5c70a766cdf629
luzh <332862394@qq.com>
2022-01-04 19:43:08 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloW2 from './components/HelloW2.vue'
+</script>
+
+<template>
+  <div>hi</div>
+  <HelloW2/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hellow2.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+hellow
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>ƒ
