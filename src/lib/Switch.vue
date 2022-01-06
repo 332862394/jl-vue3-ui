527c8014a136d56b63e242f809a5307f5b2736b8
luzh <332862394@qq.com>
2022-01-06 13:56:14 +0800

    使用v-model

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -7,12 +7,12 @@
 import { ref } from "vue";
 export default {
   props: {
-    value: [Boolean, String],
+    value: Boolean,
   },
   setup(props, context) {
     const checked = ref(false);
     const toggle = () => {
-      context.emit("input", !props.value);
+      context.emit("update:value", !props.value);
     };
     return { toggle };
   },
