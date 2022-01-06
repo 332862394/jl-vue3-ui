bccf7edc741cb73e98a6b309e7ecdfacb84bfc3b
luzh <332862394@qq.com>
2022-01-06 09:23:36 +0800

    完善aside切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { ref, provide } from "vue";
+const width = document.documentElement.clientWidth;
 
-const menuVisible = ref(false);
+const menuVisible = ref(width <= 500 ? false : true);
 provide("menuVisible", menuVisible);
 </script>
 
