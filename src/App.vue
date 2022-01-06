22561b1f10191d5e530ff2ab32b2b6c0683d3369
luzh <332862394@qq.com>
2022-01-06 09:38:36 +0800

    路由切换之后关闭aside;2C

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { ref, provide } from "vue";
+import { router } from "./router";
 const width = document.documentElement.clientWidth;
 
 const menuVisible = ref(width <= 500 ? false : true);
 provide("menuVisible", menuVisible);
+router.afterEach(() => {
+  if (width <= 500) {
+    menuVisible.value = false;
+  }
+});
 </script>
 
 <template>
