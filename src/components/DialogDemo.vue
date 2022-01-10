7dc59da283e4f286978ca04c9d87587e34c16ba3
luzh <332862394@qq.com>
2022-01-10 22:29:03 +0800

    支持content和title

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,15 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog :visible="x"></Dialog>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  >
+    <div>111</div>
+    <div>222</div>
+  </Dialog>
 </template>
 
 <script lang="ts">
@@ -19,7 +27,11 @@ export default {
     const toggle = () => {
       x.value = !x.value;
     };
-    return { x, toggle };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
   },
 };
 </script>
