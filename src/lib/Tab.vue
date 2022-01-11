074ad8ba6e20ad96a5eb8f78805ecd3d5fae0fb3
luzh <332862394@qq.com>
2022-01-11 08:48:53 +0800

    创建Tab和Tabs组件

---
+++ src/lib/Tab.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Tab组件</div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Tabs组件</div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
