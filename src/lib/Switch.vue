54b30c7cc234631a9a1ebf7278f83d0b9c0ebbc2
luzh <332862394@qq.com>
2022-01-13 14:55:50 +0800

    添加rollup文件,修改package文件,上传npm

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,20 +1,18 @@
 <template>
   <button
     class="gulu-switch"
-    :class="{ 'gulu-checked': value }"
     @click="toggle"
+    :class="{ 'gulu-checked': value }"
   >
     <span></span>
   </button>
 </template>
 <script lang="ts">
-import { ref } from "vue";
 export default {
   props: {
     value: Boolean,
   },
   setup(props, context) {
-    const checked = ref(false);
     const toggle = () => {
       context.emit("update:value", !props.value);
     };
@@ -22,9 +20,10 @@ export default {
   },
 };
 </script>
-<style lang="scss" >
+
+<style lang="scss">
 $h: 22px;
-$h2: (22px-4px);
+$h2: $h - 4px;
 .gulu-switch {
   height: $h;
   width: $h * 2;
@@ -39,16 +38,15 @@ $h2: (22px-4px);
     height: $h2;
     width: $h2;
     background: white;
-    border-radius: $h2/2;
+    border-radius: $h2 / 2;
     transition: all 250ms;
   }
   &.gulu-checked {
     background: #1890ff;
     > span {
-      left: 23px;
+      left: calc(100% - #{$h2} - 2px);
     }
   }
-
   &:focus {
     outline: none;
   }
